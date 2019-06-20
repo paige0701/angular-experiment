2f3d5c73b98ca9db3f83565e0ba81653721306ff
paige <paigechoi0701@gmail.com>
2019-06-20 14:53:57 +0900

    add descripion of multiple content ng-content

--- src/app/ng-content/multiple-transclusion/multiple-transclusion.component.ts
+++ src/app/ng-content/multiple-transclusion/multiple-transclusion.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-multiple-transclusion',
   template: `
+    
+    <div style="width: 50%">팝업을 만든다고 가정을 했을 때 아래처럼 heading, description, btn 값을 직접 전달할 수도 있다. 간단히 보여주기식 컴포넌트를 만들 때 ng-content를 사용한다면 편리할 것 같다.</div>
+    
     <app-multiple-content (cancel)="onCancel()">
       <div heading>Are you sure you want to delete?</div>
       <div description>Related items might all be deleted</div>
